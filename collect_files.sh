80d2eed827916bb7f1bae3af32ff550fc9716553
yyaroslavskiy <aznn.yrn@gmail.com>
2025-04-26 22:27:30 +0300

    v5

--- collect_files.sh
+++ collect_files.sh
@@ -4,23 +4,19 @@ copy(){
     local name=$(basename "$1")
     local i=1
     local file_route="$2/$name"
-    while [[ -e "$file_route" ]]; do
+    while [[ -e "$2" ]] do
         file_route="$2/${name}.$i"
         ((i++))
     done
     cp -p "$1" "$file_route"
 }
 
-input="$1"
-output="$2"
-depth="$4"
-
-if [ -n "$depth" ]; then
-    find "$input" -maxdepth "$depth" -type f -print0 | while read -r -d '' object; do
-    	copy "$object" "$output"
+if [ -z "$3" ]; then
+    find "$1" -type f | while read r -d '' object; do
+    copy "$object" "$2"
     done
 else
-    find "$input" -type f -print0 | while read r -d '' object; do
-    	copy "$object" "$output"
+    find "$1" -maxdepth "$3" -type f | while read r -d '' object; do
+    copy "$object" "$2"
     done
 fi
