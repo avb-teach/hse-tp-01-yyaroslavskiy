b053880b5a1a830edc9b6ca91e007c92ea7ccaa8
yyaroslavskiy <aznn.yrn@gmail.com>
2025-04-26 22:46:51 +0300

    v6

--- collect_files.sh
+++ collect_files.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-copy(){
+copy() {
     local name=$(basename "$1")
     local i=1
     local file_route="$2/$name"
-    while [[ -e "$file_route" ]] do
+    
+    while [[ -e "$file_route" ]]; do
         file_route="$2/${name}.$i"
-        let i++
+        ((i++))
     done
+    
     cp -p "$1" "$file_route"
 }
 
 if [ -z "$3" ]; then
-    find "$1" -type f | while read -r -d '' object; do
-    copy "$object" "$2"
+    find "$1" -type f -print0 | while IFS= read -r -d '' object; do
+        copy "$object" "$2"
     done
 else
-    find "$1" -maxdepth "$3" -type f | while read -r -d '' object; do
-    copy "$object" "$2"
+    find "$1" -maxdepth "$3" -type f -print0 | while IFS= read -r -d '' object; do
+        copy "$object" "$2"
     done
 fi
