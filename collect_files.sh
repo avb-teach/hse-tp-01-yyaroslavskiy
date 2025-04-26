cb560cb860984872ad2b6179ba321a53c0fa8e1a
yyaroslavskiy <aznn.yrn@gmail.com>
2025-04-26 21:35:46 +0300

    modification

--- collect_files.sh
+++ collect_files.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-if [ -z "$3" ]; then
-    find "$1" -type f -exec cp {} "$2" \;
-else
-    find "$1" -maxdepth "$3" -type f -exec cp {} "$2" \;
-fi
 
+copy(){
+    local name=$(basename "$1")
+    local i=1
+    local file_route="$2/$name"
+    while [[ -e "$2" ]] do
+        file_route="$2/${name}.$i"
+        let i++
+    done
+    cp -p "$1" "$file_route"
+}
+
+if [ -z "$3" ]; then
+    find "$1" -type f | while read r -d '' object; do
+    copy "$object" "$2"
+    done
+else
+    find "$1" -maxdepth "$3" -type f | while read r -d '' object; do
+    copy "$object" "$2"
+    done
+fi
